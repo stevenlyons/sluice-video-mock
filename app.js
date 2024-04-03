2e4b8694cf8b3dd8da56bb6fb76dd4354cdcbbdf
stevenlyons <slyons@mux.com>
2024-04-02 22:26:43 -0700

    Gets the basics working
    * Playback
    * Start time
    * Rebuffering
    * Error

--- app.js
+++ app.js
@@ -7,17 +7,15 @@ const extname = path.extname;
 // Segment length is 5 seconds
 const segmentLength = 5;
 
-// try GET /app.js
-
 app.use(async ctx => {
     const filepath = path.dirname(ctx.path);
     const filename = path.basename(ctx.path);
 
-    console.log(`none: path: ${filepath} name: ${filename}`);  
+    //console.log(`none: path: ${filepath} name: ${filename}`);  
 
     if (shouldIgnoreRequest(filename)) return;
     
-    const operations = parseSpec(filepath);
+    const operations = parseSpecification(filepath);
 
     switch (checkRequestType(filename)) {
       case 'media':
@@ -51,67 +49,7 @@ app.use(async ctx => {
 
 if (!module.parent) app.listen(3000);
 
-async function processSegment(ctx, timeline, time) {
-  if (!timeline || time === undefined) return;
-
-  const segmentNum = Math.ceil(time / segmentLength);
-  const segment = timeline.find((el) => el.segment === segmentNum);
-console.dir(segment);
-
-  const filename = segmentNum > 0 ? '0.ts' : '0.ts';
-
-  if (segment) {
-    // Delayed playback for startup or rebuffer
-    if (segment.delay > 0) {
-      await sleep(segment.delay);
-      await outputFile(ctx, '/media', filename);
-    } 
-
-    // Throw an Error
-    if (segment.error > 0) {
-      await outputError(ctx, segment.error);
-    }
-  } else {
-    // Nominal playback
-    await outputFile(ctx, '/media', filename);
-  }
-};
-
-async function generateMediaPlaylist(ctx, filepath, filename) {
-  await outputFile(ctx, '/media', filename);
-}
-
-async function generateRendition(ctx, medialength) {
-  const start = 
-`#EXTM3U
-#EXT-X-VERSION:3
-#EXT-X-TARGETDURATION:6
-#EXT-X-PLAYLIST-TYPE:VOD`;
-  const end = `\n#EXT-X-ENDLIST`;
-  var segments = '';
-
-  for (let i = 0; i < medialength / 5; i++) {
-    segments += '\n' + 
-`#EXTINF:5,
-${i}.ts`;
-  }
-
-  ctx.type = 'application/x-mpegURL'
-  ctx.body = start + segments + end;
-}
-
-function calculateElapsedPlayheadTime(filename) {
-  if (!filename) return;
-
-  // Method:
-  // Use the current segment request to figure out where in the rendition playlist
-  // the player currently is. Each segment is 5sec and each segment is named in 
-  // integer order so we can multiply those together.
-  const currentSegment = path.basename(filename, path.extname(filename));
-  if (Number.isNaN(parseInt(currentSegment))) return;
-
-  return parseInt(currentSegment) * segmentLength;
-}
+// Request handlers
 
 function shouldIgnoreRequest(filename) {
   const ignore = ['favicon.ico', 'apple-touch-icon-precomposed.png', 'apple-touch-icon.png']
@@ -135,9 +73,58 @@ function checkRequestType(filename) {
   }
 }
 
+async function generateMediaPlaylist(ctx, filepath, filename) {
+  await outputFile(ctx, '/media', filename);
+}
+
+async function generateRendition(ctx, medialength) {
+  const start = 
+`#EXTM3U
+#EXT-X-VERSION:3
+#EXT-X-TARGETDURATION:6
+#EXT-X-PLAYLIST-TYPE:VOD`;
+  const end = `\n#EXT-X-ENDLIST`;
+  var segments = '';
+
+  for (let i = 0; i < medialength / segmentLength; i++) {
+    segments += 
+`\n#EXTINF:5,
+${i}.ts`;
+  }
+
+  outputString(ctx, 'application/x-mpegURL', start + segments + end);
+}
+
+async function processSegment(ctx, timeline, time) {
+  if (!timeline || time === undefined) return;
+
+  const segmentNum = Math.ceil(time / segmentLength);
+  const segment = timeline.find((el) => el.segment === segmentNum);
+
+  const filename = '0.ts';
+
+  if (segment) {
+    // Delayed playback for startup or rebuffer
+    if (segment.delay > 0) {
+      await sleep(segment.delay);
+      await outputFile(ctx, '/media', filename);
+    } 
+
+    // Throw an Error
+    if (segment.error > 0) {
+      outputError(ctx, segment.error);
+    }
+  } else {
+    // Nominal playback
+    await outputFile(ctx, '/media', filename);
+  }
+};
+
+// Specification parsing
+
 // Parses the path to pull out the operations. Changes:
-// '/s5-p30-r10/' to [{ op: 'startup', time: 5 }, { op: 'playback', time: 30 }, { op: 'rebuffer', time: 10 }]
-function parseSpec(path) {
+// '/s5-p30-r10/' to [{ op: 'startup', delay: 5 }, { op: 'playback', time: 30 }, { op: 'rebuffer', delay: 10 }]
+function parseSpecification(path) {
   if (!path) return;
 
   const opString = path.startsWith('/') ? path.substring(1) : path;
@@ -156,58 +143,6 @@ function parseSpec(path) {
   return ops;
 }
 
-// Processes the operation specification and generates the actions for each segment.
-// Outputs an array with the action for each segment file. The filename is the index 
-// into the segment e.g. [{ segment: 0, delay: X }] is for 0.ts, etc.
-// Currently, 'delay' and 'failure' are the only commands: 
-//   [{ segment: 0, delay: 5 }, { segment: 5, failure: 404 }, ...]
-function createSegmentTimeline(operations) {
-  var timeline = [];
-  var currentSegment = 0;
-
-  if (!operations) return timeline;
-
-  for (let i = 0; i < operations.length; i++) {
-    switch (operations[i].op) {
-      case 'startup':
-        // Hardcode startup to first segment
-        timeline.push({ segment: 0, delay: operations[i].time });
-        currentSegment++;
-        break;
-      case 'playback':
-        // No action to take for nominal playback
-        currentSegment += Math.ceil(operations[i].time / segmentLength);
-        break;
-      case 'rebuffer':
-        timeline.push({ segment: currentSegment, delay: operations[i].time });
-        currentSegment++;
-        break;
-      case 'error':
-        timeline.push({ 
-          segment: currentSegment, 
-          error: (operations[i].code ? operations[i].code : '500') 
-        });
-        currentSegment++;
-        break;
-      default:
-        break;
-    }
-  }
-
-  return timeline;
-}
-
-function calculateMediaLength(operations) {
-  if (!operations) return;
-
-  return operations.reduce((total, operation) => 
-    total + roundToSegmentSize((operation.op === 'playback' ? operation.time : 0)), 0);
-}
-
-function roundToSegmentSize(length) {
-  return Math.ceil(length / segmentLength) * segmentLength;
-}
-
 function parseOp(opString) {
   var op = {};
 
@@ -215,15 +150,15 @@ function parseOp(opString) {
   switch (opString.charAt(0)) {
     case 's':
         op['op'] = 'startup';
-        op['time'] = data;
+        op['delay'] = data || 5; // Default to 5 seconds
         break;
     case 'p':
         op['op'] = 'playback';
-        op['time'] = data;
+        op['time'] = data || 30; // Default to 30 seconds
         break;
     case 'r':
         op['op'] = 'rebuffer';
-        op['time'] = data;
+        op['delay'] = data || 30; // Default to 30 seconds
         break;
     case 'e':
         op['op'] = 'error';
@@ -240,14 +175,96 @@ function parseData(dataString) {
   }
 }
 
+// Processes the operation specification and generates the actions for each segment.
+// Outputs an array with the action for each segment file. The filename is the index 
+// into the segment e.g. [{ segment: 0, delay: X }] is for 0.ts, etc.
+// Currently, 'delay' and 'failure' are the only commands: 
+//   [{ segment: 0, delay: 5 }, { segment: 5, failure: 404 }, ...]
+function createSegmentTimeline(operations) {
+  var timeline = [];
+  var currentSegment = 0;
+
+  if (!operations) return timeline;
+
+  for (let i = 0; i < operations.length; i++) {
+    switch (operations[i].op) {
+      case 'startup':
+        // Hardcode startup to first segment
+        timeline.push({ segment: 0, delay: operations[i].delay });
+        currentSegment++;
+        break;
+      case 'playback':
+        // No action to take for nominal playback
+        currentSegment += Math.ceil(operations[i].time / segmentLength);
+        break;
+      case 'rebuffer':
+        timeline.push({ segment: currentSegment, delay: operations[i].delay });
+        // the current segment doesn't increment because rebuffering requires 
+        // another action after it
+        break;
+      case 'error':
+        // Check last segment and see if it has the same segment number.
+        // Bascially, this will merge a rebuffering and errored segment.
+        const el = timeline.at(-1);
+        if (el && el.segment === currentSegment) {
+          el.error = (operations[i].code ? operations[i].code : '500') 
+        } else {
+          timeline.push({ 
+            segment: currentSegment, 
+            error: (operations[i].code ? operations[i].code : '500')
+          });
+        }
+        currentSegment++;
+        break;
+      default:
+        break;
+    }
+  }
+  
+  return timeline;
+}
+
+function calculateElapsedPlayheadTime(filename) {
+  if (!filename) return;
+
+  // Method:
+  // Use the current segment request to figure out where in the rendition playlist
+  // the player currently is. Each segment is 5sec and each segment is named in 
+  // integer order so we can multiply those together.
+  const currentSegment = path.basename(filename, path.extname(filename));
+  if (Number.isNaN(parseInt(currentSegment))) return;
+
+  return parseInt(currentSegment) * segmentLength;
+}
+
+function calculateMediaLength(operations) {
+  if (!operations) return;
+
+  // Playback event length is as long as specified (rounded to the segment length), 
+  // rebuffering has length 0 because it depends on other actions for segments, 
+  // all other events are one segment of playback.
+  return operations.reduce((total, operation) => 
+    total + roundToSegmentSize((operation.op === 'playback' ? operation.time : 
+      (operation.op === 'rebuffer' ? 0 : segmentLength))), 0);
+}
+
+function roundToSegmentSize(length) {
+  return Math.ceil(length / segmentLength) * segmentLength;
+}
+
+// Output
+
+function outputError(ctx, code) {
+  ctx.throw(code);
+}
+
 async function outputFile(ctx, filepath, filename) {
   const fpath = path.join(__dirname, filepath, filename);
   const fstat = await stat(fpath);
 
   if (fstat.isFile()) {
     const temp = extname(fpath);
-    var ext = temp.charAt(0) === '.' ? temp.substring(1) : temp;
-    ext = ext === 'm3u8' ? 'application/x-mpegURL' : ext;
+    var ext = temp.charAt(0) === '.' ? extractMimetype(temp.substring(1)) : temp;
 
     ctx.type = ext;
     ctx.length = fstat.size;
@@ -255,13 +272,16 @@ async function outputFile(ctx, filepath, filename) {
   }
 }
 
-async function outputError(ctx, code) {
-  ctx.throw(code);
+function outputString(ctx, type, body) {
+  ctx.type = type;
+  ctx.body = body;
 }
 
-/**
- * thunkify stat
- */
+function extractMimetype(ext) {
+  return ext === 'm3u8' ? 'application/x-mpegURL' : ext;
+}
+
+// System functions
 
 function stat(file) {
   return new Promise(function(resolve, reject) {
